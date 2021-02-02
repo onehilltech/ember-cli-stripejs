36436d5f85d97a521c2f6337fee72b1e5c7152ec
James Hill <james@onehilltech.com>
2021-02-01 23:47:56 -0500

    fix: added missing attributes to payment intent

--- addon/models/stripe-payment-intent.js
+++ addon/models/stripe-payment-intent.js
@@ -1,5 +1,33 @@
-import Model from '@ember-data/model';
+import Model, { attr } from '@ember-data/model';
 
 export default class StripePaymentIntentModel extends Model {
+  @attr ('number')
+  amount;
 
+  @attr
+  canceledAt;
+
+  @attr
+  cancellationReason;
+
+  @attr
+  captureMethod;
+
+  @attr
+  clientSecret;
+
+  @attr
+  confirmationMethod;
+
+  @attr
+  currency;
+
+  @attr
+  description;
+
+  @attr
+  receiptEmail;
+
+  @attr
+  status;
 }
