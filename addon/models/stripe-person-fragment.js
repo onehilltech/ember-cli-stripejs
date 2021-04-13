fa41c52e701141bfd434c8b64923fffab4bd3874
James Hill <james@onehilltech.com>
2021-04-13 18:47:43 -0400

    fix: redefined person fragment

--- addon/models/stripe-person-fragment.js
+++ addon/models/stripe-person-fragment.js
@@ -1,5 +1,67 @@
-import MF from 'ember-data-model-fragments';
+import MF, {fragment} from 'ember-data-model-fragments';
+import { isPresent } from '@ember/utils';
+import { attr } from "@ember-data/model";
 
-import {makePerson} from "./stripe-person";
+export default class StripePersonFragment extends MF.Fragment {
+  @attr ('stripe-date')
+  created;
 
-export default makePerson (MF.Fragment);
+  @attr
+  account;
+
+  @attr
+  firstName;
+
+  @attr
+  lastName;
+
+  set name (value) {
+    if (isPresent (value)) {
+      [this.firstName, this.lastName] = value.split (' ');
+    }
+    else {
+      this.firstName = this.lastName = null;
+    }
+  }
+
+  get name () {
+    let parts = [];
+
+    if (isPresent (this.firstName)) {
+      parts.push (this.firstName);
+    }
+
+    if (isPresent (this.lastName)) {
+      parts.push (this.lastName);
+    }
+
+    return parts.join (' ');
+  }
+
+  @attr
+  email;
+
+  @attr
+  phone;
+
+  @fragment('stripe-address')
+  address;
+
+  @fragment('stripe-dob')
+  dob;
+
+  @attr
+  metadata;
+
+  @fragment('stripe-requirements', { serialize: false })
+  requirements;
+
+  @fragment('stripe-relationship')
+  relationship;
+
+  @attr
+  ssnLast4;
+
+  @attr ({ serialize: false })
+  ssnLast4Provided;
+}
