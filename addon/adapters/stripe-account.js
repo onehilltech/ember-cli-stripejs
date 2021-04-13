c9813050c768eb7e8043061f48695dc628253502
James Hill <james@onehilltech.com>
2021-04-13 12:13:41 -0400

    feat: stripe account adapter allows you to customize the base url from a model

--- addon/adapters/stripe-account.js
+++ addon/adapters/stripe-account.js
@@ -4,16 +4,44 @@ import { isPresent } from '@ember/utils';
 export default class StripeAccountAdapter extends ApplicationAdapter {
   urlForFindRecord (id, modelName, snapshot) {
     let { adapterOptions } = snapshot;
-    let { baseModel, path = 'merchant' } = adapterOptions;
+    let { baseModel, path = 'account' } = adapterOptions;
 
     if (isPresent (baseModel)) {
-      let adapter = baseModel.store.adapterFor (baseModel.constructor.modelName);
-      let url = adapter.urlForFindRecord (baseModel.id,  baseModel.constructor.modelName, baseModel);
-
-      return `${url}/${path}`;
+      return this._computeUrlFromBaseRecord (baseModel, path);
     }
     else {
       return super.urlForFindRecord (...arguments);
     }
+  }
+
+  urlForUpdateRecord (id, modelName, snapshot) {
+    let { adapterOptions } = snapshot;
+    let { baseModel, path = 'account' } = adapterOptions;
+
+    if (isPresent (baseModel)) {
+      return this._computeUrlFromBaseRecord (baseModel, path);
+    }
+    else {
+      return super.urlForFindRecord (...arguments);
+    }
+  }
+
+  urlForDeleteRecord (id, modelName, snapshot) {
+    let { adapterOptions } = snapshot;
+    let { baseModel, path = 'account' } = adapterOptions;
+
+    if (isPresent (baseModel)) {
+      return this._computeUrlFromBaseRecord (baseModel, path);
+    }
+    else {
+      return super.urlForFindRecord (...arguments);
+    }
+  }
+
+  _computeUrlFromBaseRecord (baseModel, path) {
+    let adapter = baseModel.store.adapterFor (baseModel.constructor.modelName);
+    let url = adapter.urlForFindRecord (baseModel.id,  baseModel.constructor.modelName, baseModel);
+
+    return `${url}/${path}`;
   }
 }
