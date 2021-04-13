14e4384417dc02b35cc58fb69fd568e31b684c08
James Hill <james@onehilltech.com>
2021-04-13 12:48:43 -0400

    fix: dasherize all payload keys

--- addon/serializers/application.js
+++ addon/serializers/application.js
@@ -1,6 +1,11 @@
 import RESTSerializer from '@ember-data/serializer/rest';
 import MongoDB from 'ember-blueprint-data/mixins/serializers/mongodb';
+import { dasherize } from '@ember/string';
 
 export default RESTSerializer.extend (MongoDB, {
-  primaryKey: 'id'
+  primaryKey: 'id',
+
+  payloadKeyFromModelName (modelName) {
+    return dasherize (modelName);
+  }
 });
