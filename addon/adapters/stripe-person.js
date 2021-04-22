f2c047b97d2e6bd5f38341a26c44547e00d326dd
James Hill <james@onehilltech.com>
2021-04-22 16:40:47 -0400

    feat: redesigned the person adapter

--- addon/adapters/stripe-person.js
+++ addon/adapters/stripe-person.js
@@ -8,35 +8,35 @@ export default class StripePersonAdapter extends ApplicationAdapter {
   }
 
   urlForCreateRecord (modelName, snapshot) {
-    return `${this.urlForAccount (snapshot)}/${this.pathForType (modelName)}`;
+    return `${this.urlFromSnapshot (snapshot)}/${this.pathForType (modelName)}`;
   }
 
   urlForFindRecord (id, modelName, snapshot) {
-    return `${this.urlForAccount (snapshot)}/${this.pathForType (modelName)}/${id}`;
+    return `${this.urlFromSnapshot (snapshot)}/${this.pathForType (modelName)}/${id}`;
   }
 
   urlForUpdateRecord (id, modelName, snapshot) {
-    return `${this.urlForAccount (snapshot)}/${this.pathForType (modelName)}/${id}`;
+    return `${this.urlFromAccount (snapshot.account)}/${this.pathForType (modelName)}/${id}`;
   }
 
   urlForDeleteRecord (id, modelName, snapshot) {
-    return `${this.urlForAccount (snapshot)}/${this.pathForType (modelName)}/${id}`;
-  }
-
-  urlForQuery (query, modelName) {
-    return `${this.urlForAccount (snapshot)}/${this.pathForType (modelName)}`;
+    return `${this.urlFromAccount (snapshot.account)}/${this.pathForType (modelName)}/${id}`;
   }
 
   urlForFindAll (modelName, snapshot) {
-    return `${this.urlForAccount (snapshot)}/${this.pathForType (modelName)}`;
+    return `${this.urlFromSnapshot (snapshot)}/${this.pathForType (modelName)}`;
   }
 
-  urlForAccount (snapshot) {
+  urlFromSnapshot (snapshot) {
     let { adapterOptions } = snapshot;
     let { account } = adapterOptions;
 
     assert ('You must pass an stripe-account model as <account> in the adapter options', isPresent (account));
 
+    return this.urlFromAccount (account);
+  }
+
+  urlFromAccount (account) {
     let adapter = account.store.adapterFor (account.constructor.modelName);
     return adapter.urlForFindRecord (account.id,  account.constructor.modelName, account);
   }
