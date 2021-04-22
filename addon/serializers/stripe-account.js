318a27a1f6499544fbacafe31239f2f364665922
James Hill <james@onehilltech.com>
2021-04-22 14:27:17 -0400

    fix: adapter failed if there were no external accounts

--- addon/serializers/stripe-account.js
+++ addon/serializers/stripe-account.js
@@ -1,9 +1,13 @@
 import ApplicationSerializer from './application';
+import { isPresent } from '@ember/utils';
 
 export default class StripeAccountSerializer extends ApplicationSerializer {
   normalizeSingleResponse (store, primaryModelClass, payload, id, requestType) {
     let { 'stripe-account': account } = payload;
-    account.external_accounts = account.external_accounts.data;
+
+    if (isPresent (account.external_accounts)) {
+      account.external_accounts = account.external_accounts.data;
+    }
 
     return super.normalizeSingleResponse (...arguments);
   }
