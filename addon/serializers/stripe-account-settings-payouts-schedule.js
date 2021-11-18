44145c7d0f9b3420b2f8f1da35e2eae373ddd093
James Hill <james@onehilltech.com>
2021-11-17 22:17:51 -0500

    fix: do not serialize payout anchors if null

--- addon/serializers/stripe-account-settings-payouts-schedule.js
+++ addon/serializers/stripe-account-settings-payouts-schedule.js
@@ -1,4 +1,25 @@
 import ApplicationSerializer from './application';
+import { isPresent } from '@ember/utils';
 
 export default class StripeAccountSettingsPayoutsScheduleSerializer extends ApplicationSerializer {
+  /**
+   * Serialize an attribute.
+   *
+   * @param snapshot
+   * @param json
+   * @param key
+   */
+  serializeAttribute(snapshot, json, key) {
+    switch (key) {
+      case 'monthlyAnchor':
+      case 'weeklyAnchor':
+        if (isPresent (snapshot.attr (key))) {
+          super.serializeAttribute (...arguments);
+        }
+        break;
+
+      default:
+        super.serializeAttribute (...arguments);
+    }
+  }
 }
