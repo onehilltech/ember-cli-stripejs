888bfa4216bc7c897f11a18f6bd73e5276f357da
James Hill <james@onehilltech.com>
2021-11-27 21:15:14 -0500

    feat: added default values to the relationships

--- addon/models/stripe-relationship.js
+++ addon/models/stripe-relationship.js
@@ -2,19 +2,19 @@ import MF from 'ember-data-model-fragments';
 import { attr } from '@ember-data/model';
 
 export default class StripeRelationshipFragment extends MF.Fragment {
-  @attr ('boolean')
+  @attr ('boolean', { defaultValue: false })
   director;
 
-  @attr ('boolean')
+  @attr ('boolean', { defaultValue: false })
   executive;
 
-  @attr ('boolean')
+  @attr ('boolean', { defaultValue: false })
   owner;
 
   @attr ('number')
   percentOwnership;
 
-  @attr ('boolean')
+  @attr ('boolean', { defaultValue: false })
   representative;
 
   @attr
