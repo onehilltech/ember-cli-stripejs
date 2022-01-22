84839cf76ffb00067a7c149cb8d5474c9e3abdde
James Hill <james@onehilltech.com>
2022-01-21 22:33:56 -0500

    fix: set target for @ember/render-modifiers

--- blueprints/ember-cli-stripejs/index.js
+++ blueprints/ember-cli-stripejs/index.js
@@ -4,7 +4,7 @@ const { Blueprint } = require ('ember-cli-blueprint-helpers');
 
 module.exports = Blueprint.extend ({
   addons: [
-    { name: '@ember/render-modifiers' },
+    { name: '@ember/render-modifiers', target: '^1.0.0' },
     { name: 'ember-api-actions' },
     { name: 'ember-blueprint-data' },
     { name: 'ember-data-model-fragments', target: '5.0.0-beta.1' },
