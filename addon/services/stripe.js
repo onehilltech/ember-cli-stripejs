5d83660f2b0237bd9762cd6b684c8ddad48100ed
James Hill <james@onehilltech.com>
2022-12-27 11:47:43 -0500

    feat: added developer mode support

--- addon/services/stripe.js
+++ addon/services/stripe.js
@@ -1,34 +1,57 @@
 import Service from '@ember/service';
 import { getOwner } from '@ember/application';
-import { get } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { isPresent } from '@ember/utils';
 import { loadStripe } from '@stripe/stripe-js';
+import { tracked } from '@glimmer/tracking';
 
-function hasCordovaPlugin () {
-  return isPresent (window.cordova) && isPresent (window.cordova.plugins) && isPresent (window.cordova.plugins.stripe);
+function hasCordovaPlugin() {
+  return (
+    isPresent(window.cordova) &&
+    isPresent(window.cordova.plugins) &&
+    isPresent(window.cordova.plugins.stripe)
+  );
 }
 
 export default class StripeService extends Service {
   @service
   store;
 
-  get config () {
-    return getOwner (this).resolveRegistration ('config:environment').stripe;
+  @tracked
+  _developerMode;
+
+  get config() {
+    return getOwner(this).resolveRegistration('config:environment').stripe;
   }
 
-  get defaultPublishableKey () {
-    return get (this.config, 'publishableKey');
+  get defaultPublishableKey() {
+    return this.config.publishableKey;
   }
 
-  get defaultApiVersion () {
-    return get (this.config, 'version');
+  get defaultApiVersion() {
+    return this.config.version;
   }
 
-  get defaultOptions () {
+  get defaultOptions() {
     return {
-      apiVersion: this.defaultApiVersion
+      apiVersion: this.defaultApiVersion,
+    };
+  }
+
+  set developerMode(developerMode) {
+    if (this._developerMode === developerMode) {
+      return;
     }
+
+    // The developer mode has changed. We are going to reset the Stripe reference
+    // just in case the user application supports developer mode.
+
+    this._developerMode = developerMode;
+    this._stripe = null;
+  }
+
+  get developerMode() {
+    return this._developerMode;
   }
 
   /**
@@ -37,9 +60,9 @@ export default class StripeService extends Service {
    * @param type          Type of element to create
    * @param options       Element options
    */
-  async createElement (type, options) {
-    const stripe = await this.getStripe ();
-    return stripe.elements ().create (type, options);
+  async createElement(type, options) {
+    const stripe = await this.getStripe();
+    return stripe.elements().create(type, options);
   }
 
   /**
@@ -48,18 +71,21 @@ export default class StripeService extends Service {
    * @param       type          Type of payment token to make
    * @param       options       The token options
    */
-  async createToken (type, options) {
-    if (type === 'bank_account' && hasCordovaPlugin ()) {
-      return new Promise ((resolve) => {
-        const success = token => resolve ({ token });
-        const failure = error => ({ error });
+  async createToken(type, options) {
+    if (type === 'bank_account' && hasCordovaPlugin()) {
+      return new Promise((resolve) => {
+        const success = (token) => resolve({ token });
+        const failure = (error) => ({ error });
 
-        window.cordova.plugins.stripe.createBankAccountToken (options, success, failure);
+        window.cordova.plugins.stripe.createBankAccountToken(
+          options,
+          success,
+          failure
+        );
       });
-    }
-    else {
-      const stripe = await this.getStripe ();
-      return stripe.createToken (type, options);
+    } else {
+      const stripe = await this.getStripe();
+      return stripe.createToken(type, options);
     }
   }
 
@@ -68,14 +94,14 @@ export default class StripeService extends Service {
    *
    * @param options
    */
-  async createPaymentMethod (options) {
-    const stripe = await this.getStripe ();
-    return stripe.createPaymentMethod (options);
+  async createPaymentMethod(options) {
+    const stripe = await this.getStripe();
+    return stripe.createPaymentMethod(options);
   }
 
-  async createPaymentRequest (options) {
-    const stripe = await this.getStripe ();
-    return stripe.paymentRequest (options);
+  async createPaymentRequest(options) {
+    const stripe = await this.getStripe();
+    return stripe.paymentRequest(options);
   }
 
   /**
@@ -85,16 +111,24 @@ export default class StripeService extends Service {
    * @param payment
    * @param options
    */
-  async confirmCardPayment (clientSecret, payment, options) {
-    const stripe = await this.getStripe ();
-    const payload = await stripe.confirmCardPayment (clientSecret, payment, options);
+  async confirmCardPayment(clientSecret, payment, options) {
+    const stripe = await this.getStripe();
+    const payload = await stripe.confirmCardPayment(
+      clientSecret,
+      payment,
+      options
+    );
 
     // Transform the payload into a stripe payment intent object.
-    const modelClass = this.store.modelFor ('stripe-payment-intent');
-    const serializer = this.store.serializerFor ('stripe-payment-intent');
-    const data = serializer.normalizeSaveResponse (this.store, modelClass, payload);
+    const modelClass = this.store.modelFor('stripe-payment-intent');
+    const serializer = this.store.serializerFor('stripe-payment-intent');
+    const data = serializer.normalizeSaveResponse(
+      this.store,
+      modelClass,
+      payload
+    );
 
-    return this.store.push (data);
+    return this.store.push(data);
   }
 
   /**
@@ -104,16 +138,24 @@ export default class StripeService extends Service {
    * @param payment
    * @param options
    */
-  async confirmCardSetup (clientSecret, payment, options) {
-    const stripe = await this.getStripe ();
-    const payload = await stripe.confirmCardSetup (clientSecret, payment, options);
+  async confirmCardSetup(clientSecret, payment, options) {
+    const stripe = await this.getStripe();
+    const payload = await stripe.confirmCardSetup(
+      clientSecret,
+      payment,
+      options
+    );
 
     // Transform the payload into a stripe payment intent object.
-    const modelClass = this.store.modelFor ('stripe-setup-intent');
-    const serializer = this.store.serializerFor ('stripe-setup-intent');
-    const data = serializer.normalizeSaveResponse (this.store, modelClass, payload);
+    const modelClass = this.store.modelFor('stripe-setup-intent');
+    const serializer = this.store.serializerFor('stripe-setup-intent');
+    const data = serializer.normalizeSaveResponse(
+      this.store,
+      modelClass,
+      payload
+    );
 
-    return this.store.push (data);
+    return this.store.push(data);
   }
 
   /**
@@ -122,9 +164,9 @@ export default class StripeService extends Service {
    *
    * @param options
    */
-  async paymentRequest (options) {
-    const stripe = await this.getStripe ();
-    return stripe.paymentRequest (options);
+  async paymentRequest(options) {
+    const stripe = await this.getStripe();
+    return stripe.paymentRequest(options);
   }
 
   /**
@@ -132,16 +174,29 @@ export default class StripeService extends Service {
    * *
    * @return {Promise<Stripe>}
    */
-  async getStripe () {
-    if (isPresent (this._stripe)) {
+  async getStripe() {
+    if (isPresent(this._stripe)) {
       return this._stripe;
     }
 
-    const { publishableKey } = this.config;
-    this._stripe = await loadStripe (publishableKey);
+    let { publishableKey } = this.config;
 
-    if (hasCordovaPlugin ()) {
-      window.cordova.plugins.stripe.setPublishableKey (publishableKey);
+    if (this._developerMode && this.config.developer) {
+      const { developer: { publishableKey: key } } = this.config;
+
+      if (isPresent (key)) {
+        publishableKey = key;
+      }
+      else {
+        console.warn ('You have enabled developer mode, but have not set the developer test key.');
+        console.warn ('Using the production key instead of the developer test key.');
+      }
+    }
+
+    this._stripe = await loadStripe(publishableKey);
+
+    if (hasCordovaPlugin()) {
+      window.cordova.plugins.stripe.setPublishableKey(publishableKey);
     }
 
     return this._stripe;
